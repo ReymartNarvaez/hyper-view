9a3b29c3e27732607f85ea0bade2552fe8b1ff8c
ReymartNarvaez <reymart_narvaez@hotmail.com>
2023-09-21 13:52:52 +0200

    Add route to browse page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { IntroText } from "./pages/IntroText";
 import { LoginPage } from "./pages/LoginPage";
+import { BrowsePage } from "./pages/BrowsePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<IntroText />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/browse" element={<BrowsePage />} />
         </Routes>
       </Router>
     </div>
